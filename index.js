a852f49e72cded360018a424e4f08db360758e4d
Alexander Lee <alex8usa8@gmail.com>
2017-10-10 15:19:46 -0700

    Registered a route that handles a GET request to a /oauth endpoint. This handles the logic of the Slack oAuth process. #8

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const localtunnel = require('localtunnel')
 const express = require('express')
+const request = require('request')
 const app = express()
 const clientId = '232154912464.253884891829'
 const clientSecret = '9a4266c986fd9a04763c825c10331d4e'
@@ -15,6 +16,30 @@ tunnel.on('close,', () => {
 })
 
 app.get('/', (req, res) => res.send('localtunnel is successful'))
+
+app.get('/oauth', (req, res) => {
+  if (!req.query.code) {
+    res.status(500)
+    res.send({'Error': 'Looks like we\'re not getting code.'})
+    console.error('Looks like we\'re not getting code.')
+  }
+  else {
+    request(
+      {
+        url: 'https://slack.com/api/oauth.access', // URL to hit
+        qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, // Query string data
+        method: 'GET' // Specify the method
+      },
+      (err, response, body) => {
+        if (err) {
+          console.error(err)
+        }
+        else {
+          res.json(body)
+        }
+      })
+  }
+})
 
 app.listen(4000, () => console.log('Server Listening on Port 4000'))
 
