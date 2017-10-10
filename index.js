168909bfc6d982b490811a789b7208277135ddf2
Alexander Lee <alex8usa8@gmail.com>
2017-10-10 15:10:53 -0700

    Copied the Client ID and Client Secret Information to index.js. #8

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const localtunnel = require('localtunnel')
 const express = require('express')
 const app = express()
-
+const clientId = '232154912464.253884891829'
+const clientSecret = '9a4266c986fd9a04763c825c10331d4e'
 const tunnel = localtunnel(4000, { subdomain: 'animebot' }, (err, tunnel) => {
   if (err) {
     console.error(err)
   }
-  console.log('Tunnel Opened on : ' + tunnel.url)
+  console.log('Tunnel Opened on: ' + tunnel.url)
 })
 
 tunnel.on('close,', () => {
   console.log('Tunnels are Closed.')
 })
 
-app.get('/', (req, res) => res.send('test'))
+app.get('/', (req, res) => res.send('localtunnel is successful'))
 
 app.listen(4000, () => console.log('Server Listening on Port 4000'))
-console.log(tunnel)
+
+console.log(clientId + ' ' + clientSecret)
