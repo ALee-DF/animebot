705d052ebcc9a79cad513fafce8df6e79b6ed78e
Alexander Lee <alex8usa8@gmail.com>
2017-10-12 14:14:20 -0700

    Created an animebot database of MongoDB server using MongoClient.connect. #1

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const localtunnel = require('localtunnel')
 const express = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
 const checklistFile = require('./buttons-checklist.js')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const app = express()
 const clientId = process.env.clientId
 const clientSecret = process.env.ClientSecret
 const animebotVerificationToken = process.env.animebot_verification_token
@@ -21,85 +21,93 @@ tunnel.on('close,', () => {
   console.log('Tunnels are Closed.')
 })
 
-app.get('/', (req, res) => res.send('localtunnel is successful'))
-
-app.get('/oauth', (req, res) => {
-  if (!req.query.code) {
-    res.sendstatus(500)
-    res.send({'Error': 'Looks like we\'re not getting code.'})
-    console.error('Looks like we\'re not getting code.')
+MongoClient.connect('mongodb://localhost/animebot', (err, db) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
   }
-  else {
-    request(
-      {
-        url: 'https://slack.com/api/oauth.access',
-        qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
-        method: 'GET'
-      },
-      (err, response, body) => {
-        if (err) {
-          console.error(err)
-          res.sendStatus(500)
-        }
-        else {
-          res.json(body)
-        }
-      })
-  }
-})
+  const app = express()
 
-app.post('/command', (req, res) => {
-  res.send('Connection to localtunnel verified. Ready for your next adventure?')
-})
+  app.get('/', (req, res) => res.send('localtunnel is successful'))
 
-function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
-  const postOptions = {
-    uri: responseURL,
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    json: JSONmessage
-  }
-  request(postOptions, (err, res, body) => {
-    if (err) {
-      console.error(err)
+  app.get('/oauth', (req, res) => {
+    if (!req.query.code) {
+      res.sendstatus(500)
+      res.send({'Error': 'Looks like we\'re not getting code.'})
+      console.error('Looks like we\'re not getting code.')
+    }
+    else {
+      request(
+        {
+          url: 'https://slack.com/api/oauth.access',
+          qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
+          method: 'GET'
+        },
+        (err, response, body) => {
+          if (err) {
+            console.error(err)
+            res.sendStatus(500)
+          }
+          else {
+            res.json(body)
+          }
+        })
     }
   })
-}
 
-app.post('/checklist', urlencodedParser, (req, res) => {
-  const reqBody = req.body
-  const responseURL = reqBody.response_url
-  if (reqBody.token !== animebotVerificationToken) {
-    console.error('Access Forbidden')
-    res.sendStatus(403)
-  }
-  else {
-    res.status(200).end()
-    sendMessageToSlackResponseURL(responseURL, buttonsChecklist)
-    console.log(req.body)
-  }
-})
+  app.post('/command', (req, res) => {
+    res.send('Connection to localtunnel verified. Ready for your next adventure?')
+  })
 
-app.post('/buttonaction', urlencodedParser, (req, res) => {
-  res.status(200).end()
-  const actionJSONPayload = JSON.parse(req.body.payload)
-  const attachmentID = Number(actionJSONPayload['attachment_id']) - 1
-  const targetIndex = buttonsChecklist['attachments'][attachmentID]['actions']
-    .findIndex(button => {
-      return button.name === actionJSONPayload.actions[0].name
+  function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
+    const postOptions = {
+      uri: responseURL,
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      json: JSONmessage
+    }
+    request(postOptions, (err, res, body) => {
+      if (err) {
+        console.error(err)
+      }
     })
-  if (actionJSONPayload.actions[0].value === 'deselected') {
-    buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['value'] = 'selected'
-    buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['style'] = 'primary'
   }
-  else {
-    buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['value'] = 'deselected'
-    buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['style'] = 'default'
-  }
-  sendMessageToSlackResponseURL(actionJSONPayload.response_url, buttonsChecklist)
-  console.log(actionJSONPayload)
-})
 
-app.listen(4000, () => console.log('Server Listening on Port 4000'))
+  app.post('/checklist', urlencodedParser, (req, res) => {
+    const reqBody = req.body
+    const responseURL = reqBody.response_url
+    if (reqBody.token !== animebotVerificationToken) {
+      console.error('Access Forbidden')
+      res.sendStatus(403)
+    }
+    else {
+      res.status(200).end()
+      sendMessageToSlackResponseURL(responseURL, buttonsChecklist)
+      console.log(req.body)
+    }
+  })
+
+  app.post('/buttonaction', urlencodedParser, (req, res) => {
+    res.status(200).end()
+    const actionJSONPayload = JSON.parse(req.body.payload)
+    const attachmentID = Number(actionJSONPayload['attachment_id']) - 1
+    const targetIndex = buttonsChecklist['attachments'][attachmentID]['actions']
+      .findIndex(button => {
+        return button.name === actionJSONPayload.actions[0].name
+      })
+    if (actionJSONPayload.actions[0].value === 'deselected') {
+      buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['value'] = 'selected'
+      buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['style'] = 'primary'
+    }
+    else {
+      buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['value'] = 'deselected'
+      buttonsChecklist['attachments'][attachmentID]['actions'][targetIndex]['style'] = 'default'
+    }
+    sendMessageToSlackResponseURL(actionJSONPayload.response_url, buttonsChecklist)
+    console.log(actionJSONPayload)
+  })
+
+  app.listen(4000, () => console.log('Server Listening on Port 4000'))
+})
