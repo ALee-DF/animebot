9313414e9c8418660276e574da8706a9506d22b9
Alexander Lee <alex8usa8@gmail.com>
2017-10-14 16:01:44 -0700

    Condensed genre selections. #2

--- buttons-checklist.js
+++ buttons-checklist.js
@@ -22,37 +22,8 @@ module.exports.buttonsChecklist = {
           style: 'default'
         },
         {
-          name: 'cars',
-          text: 'Cars',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
           name: 'comedy',
           text: 'Comedy',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'dementia',
-          text: 'Dementia',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'demons',
-          text: 'Demons',
           type: 'button',
           value: 'deselected',
           style: 'default'
@@ -65,44 +36,8 @@ module.exports.buttonsChecklist = {
           style: 'default'
         },
         {
-          name: 'ecchi',
-          text: 'Ecchi',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'fantasy',
-          text: 'Fantasy',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'game',
-          text: 'Game',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'harem',
-          text: 'Harem',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'hentai',
-          text: 'Hentai',
+          name: 'erotica',
+          text: 'Erotica',
           type: 'button',
           value: 'deselected',
           style: 'default',
@@ -114,10 +49,18 @@ module.exports.buttonsChecklist = {
             ok_text: 'Yes',
             dismiss_text: 'No'
           }
-        },
+        }
+      ]
+    },
+    {
+      fallback: 'You are unable to choose a genre',
+      callback_id: 'genre',
+      color: '#3AA3E3',
+      attachment_type: 'default',
+      actions: [
         {
-          name: 'historical',
-          text: 'Historical',
+          name: 'fantasy',
+          text: 'Fantasy',
           type: 'button',
           value: 'deselected',
           style: 'default'
@@ -130,28 +73,6 @@ module.exports.buttonsChecklist = {
           style: 'default'
         },
         {
-          name: 'josei',
-          text: 'Josei',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'kids',
-          text: 'Kids',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
           name: 'magic',
           text: 'Magic',
           type: 'button',
@@ -159,58 +80,8 @@ module.exports.buttonsChecklist = {
           style: 'default'
         },
         {
-          name: 'martial Arts',
-          text: 'Martial Arts',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'mecha',
-          text: 'Mecha',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'military',
-          text: 'Military',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'music',
-          text: 'Music',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
           name: 'mystery',
           text: 'Mystery',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'parody',
-          text: 'Parody',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'police',
-          text: 'Police',
           type: 'button',
           value: 'deselected',
           style: 'default'
@@ -238,101 +109,15 @@ module.exports.buttonsChecklist = {
           style: 'default'
         },
         {
-          name: 'samurai',
-          text: 'Samurai',
+          name: 'science fiction',
+          text: 'Science Fiction',
           type: 'button',
           value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'school',
-          text: 'School',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'sci-fi',
-          text: 'Sci-Fi',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'seinen',
-          text: 'Seinen',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'shoujo',
-          text: 'Shoujo',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'shoujo Ai',
-          text: 'Shoujo Ai',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'shounen',
-          text: 'Shounen',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'shounen Ai',
-          text: 'Shounen Ai',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
+          style: 'default',
         },
         {
           name: 'slice of life',
           text: 'Slice of Life',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        }
-      ]
-    },
-    {
-      fallback: 'You are unable to choose a genre',
-      callback_id: 'genre',
-      color: '#3AA3E3',
-      attachment_type: 'default',
-      actions: [
-        {
-          name: 'space',
-          text: 'Space',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'sports',
-          text: 'Sports',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'super power',
-          text: 'Super Power',
           type: 'button',
           value: 'deselected',
           style: 'default'
@@ -360,22 +145,8 @@ module.exports.buttonsChecklist = {
       attachment_type: 'default',
       actions: [
         {
-          name: 'vampire',
-          text: 'Vampire',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'yaoi',
-          text: 'Yaoi',
-          type: 'button',
-          value: 'deselected',
-          style: 'default'
-        },
-        {
-          name: 'yuri',
-          text: 'Yuri',
+          name: 'tournament',
+          text: 'Tournament',
           type: 'button',
           value: 'deselected',
           style: 'default'
