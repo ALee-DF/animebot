9c01e85caa88c93f961784583bb4d6aea2363eb3
Alexander Lee <alex8usa8@gmail.com>
2017-10-18 23:52:46 -0700

    Parsed the data using xml2js and returns a promise to provide the raw data. #2

--- getManyAnime.js
+++ getManyAnime.js
@@ -1,11 +1,21 @@
 const request = require('request')
+const parseString = require('xml2js').parseString
+
 function getAnnAnimeInfo(id) {
   return new Promise(function (resolve, reject) {
     request('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=' + id, (err, response, body) => {
       if (err) return reject(err)
-      console.log(body)
+      parseString(body, (err, result) => {
+        if (err) {
+          return resolve(null)
+        }
+        if (!result.ann.anime) {
+          return resolve(null)
+        }
+        resolve(result.ann.anime[0])
+      })
     })
   })
 }
 
-console.log(getAnnAnimeInfo(15784))
+getAnnAnimeInfo(15784).then(value => console.log(value))
