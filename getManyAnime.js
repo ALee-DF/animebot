7894bcea6969affb4bfaf51f70f49727920af7bc
Alexander Lee <alex8usa8@gmail.com>
2017-10-18 23:40:24 -0700

    Used the request module to retrieve data from the Anime News Network site from the specified id. #2

--- getManyAnime.js
+++ getManyAnime.js
@@ -1,6 +1,11 @@
+const request = require('request')
 function getAnnAnimeInfo(id) {
   return new Promise(function (resolve, reject) {
+    request('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=' + id, (err, response, body) => {
+      if (err) return reject(err)
+      console.log(body)
+    })
   })
 }
 
-console.log(getAnnAnimeInfo(1))
+console.log(getAnnAnimeInfo(15784))
